ffe1a88623dd80f6b971e7431b6177d4265fbd17
Facusan2016 <facusan2016@gmail.com>
2024-03-11 16:31:49 -0300

    fix gsap animations on mobile for projects

--- src/views/ProjectsView.jsx
+++ src/views/ProjectsView.jsx
@@ -2,6 +2,8 @@ import { Project } from '../components/Projects/Project'
 import Lositur from './ProjectsImages/Lositur.png'
 import Gymlink from './ProjectsImages/Gymlink.jpg'
 import BeanScene from './ProjectsImages/BeanScene.png'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const pConfig = [
   {
@@ -16,7 +18,7 @@ const pConfig = [
     id: 2,
     title: 'Gymlink',
     description: 'Gymlink is an integral tool made for manage a gym as a final project for Holberton School where I developed the project manager and frontend developer roles.',
-    webLink: 'www.lositur.com.uy',
+    webLink: 'https://gymlink.vercel.app/',
     repoLink: 'https://github.com/HolbertonFinalProjectFG/gymlink_frontend',
     img: Gymlink
   },
@@ -25,14 +27,29 @@ const pConfig = [
     title: 'BeanScene',
     description: 'BeanScene is the first project that I made following a figma design, it simulates a virtual store that has a cart, using Redux and ReactJS',
     webLink: 'https://beanscene.netlify.app/about',
-    repoLink: 'www.lositur.com.uy',
+    repoLink: 'https://beanscene.netlify.app/',
     img: BeanScene
   }
 ]
 
 export const ProjectsView = () => {
+  useGSAP(() => {
+    gsap.fromTo('.project', {
+      minHeight: 0
+    }, {
+      minHeight: '12rem',
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: '.project-section',
+        start: '33.3% bottom',
+        end: '120% bottom',
+        scrub: 1
+      }
+    })
+  })
+
   return (
-    <section className="w-svh h-svh relative overflow-hidden flex flex-col items-center py-10 justify-evenyl gap-10 bg-light-bg_snd">
+    <section className="project-section w-svh min-h-svh relative overflow-hidden flex flex-col p-5 items-center py-10 justify-evenyl gap-10 bg-light-bg_snd">
       <div>
         <h2 className='text-3xl font-grozen p-10 py-0 text-light-pr'>Projects</h2>
       </div>
