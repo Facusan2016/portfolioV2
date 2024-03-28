09cd07723d91846d60fee08d890376bcb3151a68
Facusan2016 <facusan2016@gmail.com>
2024-03-28 12:35:42 -0300

    adapted navbar on Desktop version

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import gsap from 'gsap'
 import hamOpen from './assets/HamMenuOpen.svg'
 import hamClosed from './assets/HamMenuClosed.svg'
@@ -26,7 +26,7 @@ export const NavBar = () => {
       w-full max-h-fit fixed top-0 z-50  py-3 px-4 bg-light-bg_snd border
       border-light-pr border-opacity-0 shadow-xl transition-all`}
     >
-      <button onClick={() => setOpenNavbar(!openNavbar)} className='relative w-5 h-6'>
+      <button onClick={() => setOpenNavbar(!openNavbar)} className='relative w-5 h-6 md:hidden'>
         <img className={`${openNavbar ? 'opacity-0' : 'opacity-100'} absolute top-0 left-0 transition-opacity duration-500`} src={ hamOpen }/>
         <img className={`${openNavbar ? 'opacity-100' : 'opacity-0'} absolute top-0 left-0 transition-opacity duration-500`} src={ hamClosed }/>
       </button>
@@ -34,12 +34,21 @@ export const NavBar = () => {
         `w-screen ${openNavbar ? 'max-h-60 !border-opacity-100' : 'max-h-0 py-0'}
         shadow-2xl top-12 left-0 duration-700 flex flex-col gap-3
         p-7 overflow-hidden transition-all font-neue uppercase text-light-snd
-        font-light text-sm`}
+        font-light text-sm md:max-h-60 md:shadow-none md:flex-row justify-end md:w-full
+        md:p-2 md:gap-8`}
       >
-        <li onClick={() => setOpenNavbar(false)} className='navBarLink'><a href='#home'>Home</a></li>
-        <li onClick={() => setOpenNavbar(false)} className='navBarLink'><a href='#about-section'>About</a></li>
-        <li onClick={() => setOpenNavbar(false)} className='navBarLink'><a href='#projects'>Projects</a></li>
-        <li onClick={() => setOpenNavbar(false)} className='navBarLink'><a href='#contactme'>Contact me</a></li>
+        <li onClick={() => setOpenNavbar(false)} className='navBarLink transition-colors md:hover:text-light-pr'>
+          <a href='#home'>Home</a>
+        </li>
+        <li onClick={() => setOpenNavbar(false)} className='navBarLink transition-colors md:hover:text-light-pr'>
+          <a href='#about-section'>About</a>
+        </li>
+        <li onClick={() => setOpenNavbar(false)} className='navBarLink transition-colors md:hover:text-light-pr'>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li onClick={() => setOpenNavbar(false)} className='navBarLink transition-colors md:hover:text-light-pr'>
+          <a href='#contactme'>Contact me</a>
+        </li>
       </ul>
     </nav>
   )
